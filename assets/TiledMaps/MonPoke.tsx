ffcdf4c8501f873b12c2ec26ce5b31334c1ac29b
sducker <scri0962@gmail.com>
2026-01-03 18:20:12 -0500

    loadermap

--- assets/TiledMaps/MonPoke.tsx
+++ assets/TiledMaps/MonPoke.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="items" tilewidth="32" tileheight="32" tilecount="5208" columns="8">
- <image source="../../../../../Descargas/pokemon_tileset_from_public_tiles_32x32_by_chaoticcherrycake_dab2byf.png" width="256" height="20832"/>
+ <image source="pokemon_tileset_from_public_tiles_32x32_by_chaoticcherrycake_dab2byf.png" width="256" height="20832"/>
 </tileset>
